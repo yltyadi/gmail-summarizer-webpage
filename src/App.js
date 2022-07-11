8f67b0d23d3e8fe11987bd6ee1434d27c708d752
yltyadi <adi.yeltay@nu.edu.kz>
2022-07-11 10:23:43 +0600

    added privacy policy with get started buttons

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import './App.css';
+// import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <div className='header-div'>
+      <div className='header-div'> 
         <div className='logo'>summarize.me</div>
         <div>
-          <a className='privacy-link' href='#'>Privacy Policy</a>
-          <button className='header-btn'>Get started</button>
+          <a href='https://www.privacypolicygenerator.info/live.php?token=WpnJKcVoaeTkPYGFMseDYVxma11fUwVT' className='privacy-link' target='_blank'>
+            Privacy Policy
+          </a>
+          <a href='https://v5.reactrouter.com/web/guides/quick-start' target='_blank'>
+            <button className='header-btn'>Get started</button>
+          </a>
         </div>
       </div>
       <div className='main-body'>
         <h1>summarize.me</h1>
         <h3 className='description'>A chrome extension that shortens long emails into concise summaries.</h3>
-        <button className='get-started-btn'>
-          <a>Get Started for Free</a>
-        </button>
+        <a href='https://v5.reactrouter.com/web/guides/quick-start' target='_blank'>
+          <button className='get-started-btn'>Get Started for Free</button>
+        </a>
       </div>
       <div className='footer-div'>
         <p>2022 &copy; summarize.me</p>
