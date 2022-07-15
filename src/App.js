f06df17d4a2a4a1cd0132dd7aed156a44ab5636f
yltyadi <adi.yeltay@nu.edu.kz>
2022-07-15 10:28:27 +0600

    added valid chrome extension link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-// import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
@@ -10,7 +9,7 @@ function App() {
           <a href='https://www.privacypolicygenerator.info/live.php?token=WpnJKcVoaeTkPYGFMseDYVxma11fUwVT' className='privacy-link' target='_blank'>
             Privacy Policy
           </a>
-          <a href='https://v5.reactrouter.com/web/guides/quick-start' target='_blank'>
+          <a href='https://chrome.google.com/webstore/detail/gmail-summarizer/kgbcbbbcmkfnpebdllbppcofhmfeabol?hl=en&authuser=3' target='_blank'>
             <button className='header-btn'>Get started</button>
           </a>
         </div>
@@ -18,7 +17,7 @@ function App() {
       <div className='main-body'>
         <h1>summarize.me</h1>
         <h3 className='description'>A chrome extension that shortens long emails into concise summaries.</h3>
-        <a href='https://v5.reactrouter.com/web/guides/quick-start' target='_blank'>
+        <a href='https://chrome.google.com/webstore/detail/gmail-summarizer/kgbcbbbcmkfnpebdllbppcofhmfeabol?hl=en&authuser=3' target='_blank'>
           <button className='get-started-btn'>Get Started for Free</button>
         </a>
       </div>
